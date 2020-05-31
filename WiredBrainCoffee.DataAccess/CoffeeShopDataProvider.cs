bea32510ea408ec9cc00cac98727e8cd16c4f293
Rick <richardx1016@gmail.com>
2020-05-31 19:41:16 -0400

    Implemented Data Access

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeShops()
+        {
+            yield return new CoffeeShop { location = "Cocoa", beansInStockInKg = 12 };
+            yield return new CoffeeShop { location = "Titusville", beansInStockInKg = 23 };
+            yield return new CoffeeShop { location = "Orlando", beansInStockInKg = 62 };
+        }
+    }
+}
---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace WiredBrainCoffee.DataAccess.Model
+{
+    class CoffeeShop
+    {
+
+
+        public string locaion { get; set; }
+        public int BeansInStockInKg { get; set; }
+    }
+}
